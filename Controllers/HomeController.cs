f063122fe845a588eac10a19592bdc9cc571a6d7
Shukhrat Doniyev <doniyevshuhrat96304@gmail.com>
2021-09-19 18:53:30 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ViewResult About()
+        {
+            return View("About");
+        }
+    }
+}
